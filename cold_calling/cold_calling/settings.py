2b9ee82a275ac49ddbe2a01749df1298b1c2f259
Shivam-Shane <sk0551460@gmail.com>
2025-03-22 00:34:11 +0530

    accessing djnago secret key from .env

--- cold_calling/cold_calling/settings.py
+++ cold_calling/cold_calling/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l=iu-dnw5q$ds1s(mdph$q#f6stqv(!^*(u$q!64pgidr!2neo'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
